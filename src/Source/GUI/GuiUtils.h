dae3a1eda37fee89e19d3331b432a6ee3fcb3141
Corentin Comte <c-comte@live.fr>
2016-11-07 21:08:03 +0100

    Internal : changed the include file to MainComponent.h

--- src/Source/GUI/GuiUtils.h
+++ src/Source/GUI/GuiUtils.h
@@ -11,9 +11,10 @@
 
 //This is where all the juce header concerning the GUI should be included.
 //This file must be included by every GUI component.
-#include "juce_graphics.h"
-#include "juce_gui_basics.h"
-#include "juce_gui_extra.h"
+/*#include "juce_graphics/juce_graphics.h"
+#include "juce_gui_basics/juce_gui_basics.h"
+#include "juce_gui_extra/juce_gui_extra.h"*/
+#include "../MainComponent.h"
 
 
 #endif /* GuiUtils_h */
